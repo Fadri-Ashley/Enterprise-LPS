7a781083789b7c9da3f904d9075c5165798fa88e
Fadri <fadriasliarga@gmail.com>
2025-09-27 14:55:16 +0700

    fix drag for different page

--- script/script1.js
+++ script/script1.js
@@ -3,31 +3,37 @@
 const list = document.querySelector(".container");
 let draggingItem = null;
 
-list.addEventListener("dragstart", (e) => {
-  draggingItem = e.target;
-  e.target.classList.add("dragging");
-});
-list.addEventListener("dragend", (e) => {
-  e.target.classList.remove("dragging");
-  document
-    .querySelectorAll(".article")
-    .forEach((item) => item.classList.remove("over"));
-  draggingItem = null;
-  saveOrder()
-});
-list.addEventListener("dragover", (e) => {
-  e.preventDefault();
-  const draggingOverItem = getDragAfterElement(list, e.clientY);
-  document
-    .querySelectorAll(".article")
-    .forEach((item) => item.classList.remove("over"));
-  if (draggingOverItem) {
-    draggingOverItem.classList.add("over");
-    list.insertBefore(draggingItem, draggingOverItem);
-  } else {
-    list.appendChild(draggingItem);
-  }
-});
+const storageKey = window.location.pathname + "_articleOrder";
+
+if (list) {
+  list.addEventListener("dragstart", (e) => {
+    draggingItem = e.target;
+    e.target.classList.add("dragging");
+  });
+  list.addEventListener("dragend", (e) => {
+    e.target.classList.remove("dragging");
+    document
+      .querySelectorAll(".article")
+      .forEach((item) => item.classList.remove("over"));
+    draggingItem = null;
+    saveOrder();
+  });
+  list.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const draggingOverItem = getDragAfterElement(list, e.clientY);
+    document
+      .querySelectorAll(".article")
+      .forEach((item) => item.classList.remove("over"));
+    if (draggingOverItem) {
+      draggingOverItem.classList.add("over");
+      list.insertBefore(draggingItem, draggingOverItem);
+    } else {
+      list.appendChild(draggingItem);
+    }
+  });
+
+  loadOrder();
+}
 
 function getDragAfterElement(container, y) {
   const draggableElements = [
@@ -49,19 +55,21 @@ function getDragAfterElement(container, y) {
 
 // Save Order to local storage
 
- function saveOrder() {
-    const order = [...list.querySelectorAll(".article")].map(el => el.dataset.id);
-    localStorage.setItem("articleOrder", JSON.stringify(order));
-  }
+function saveOrder() {
+  const order = [...list.querySelectorAll(".article")].map(
+    (el) => el.dataset.id
+  );
+  localStorage.setItem(storageKey, JSON.stringify(order));
+}
 
-  function loadOrder() {
-    const order = JSON.parse(localStorage.getItem("articleOrder"));
-    if (!order) return;
+function loadOrder() {
+  const order = JSON.parse(localStorage.getItem(storageKey));
+  if (!order) return;
 
-    order.forEach(id => {
-      const el = document.querySelector(`.article[data-id='${id}']`);
-      if (el) list.appendChild(el);
-    });
-  }
+  order.forEach((id) => {
+    const el = document.querySelector(`.article[data-id='${id}']`);
+    if (el) list.appendChild(el);
+  });
+}
 
-  loadOrder();
+loadOrder();
